26e1ef4a6cd2ee240c678c77994e112b960bddd8
stefanin <stefano.cornelli@gmail.com>
2018-10-29 17:39:58 +0100

    Update wifiscan.py
    insert loop 30 second

--- wifiscan.py
+++ wifiscan.py
@@ -1,20 +1,29 @@
 import subprocess
-risultato = subprocess.check_output(["netsh", "wlan", "show", "networks", "mode=BSSID"])
-risultato = risultato.decode("ascii") 
-risultato = risultato.replace("\r","")
-ssidScan = risultato.split("\n")
+import time
+def scansione():
+
+        risultato = subprocess.check_output(["netsh", "wlan", "show", "networks", "mode=BSSID"])
+        risultato = risultato.decode("ascii") 
+        risultato = risultato.replace("\r","")
+        ssidScan = risultato.split("\n")
+        
+        ssidScan = ssidScan[4:]
+        ssids = []
+        x = 0
+
+        while x < len(ssidScan):
+            if "BSSID" in ssidScan[x]:
+                print(ssidScan[x],end="")
+            else:
+            
+                if "SSID" in ssidScan[x]:
+                    print(ssidScan[x])
+                if "Signal" in ssidScan[x]:
+                    print(ssidScan[x])
+            x += 1
+
 print ("CLEME wifi scanner r1.0")
-ssidScan = ssidScan[4:]
-ssids = []
-x = 0
-while x < len(ssidScan):
-    if "BSSID" in ssidScan[x]:
-        print(ssidScan[x],end="")
-    else:
-     
-        if "SSID" in ssidScan[x]:
-            print(ssidScan[x])
-        if "Signal" in ssidScan[x]:
-            print(ssidScan[x])
-    x += 1
- 
+while True:
+    scansione()
+    time.sleep(30)
+    print("_________________________________________________________________________________") 
